5020d73ae7ce5903758678a5b428d8b917c8dd26
Paul Mohr <github@sopamo.de>
2014-06-02 08:58:22 +0200

    inital package commit

---
+++ src/Sopamo/Laravel4Seo/Laravel4SeoServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Sopamo\Laravel4Seo;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel4SeoServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sopamo/laravel4-seo');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
